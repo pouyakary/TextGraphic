27f1113a0871d85ea257fbae682dfe2837ea41f3
Pouya Kary ✨ <pouya@kary.us>
2021-07-14 00:00:02 +0430

    more beautiful

--- textkit/playgrounds/graph.ts
+++ textkit/playgrounds/graph.ts
@@ -12,6 +12,8 @@
 
     import * as TextKit from
         "../source"
+    import * as Tools from
+        "./tools"
 
 //
 // ─── RENDERER ───────────────────────────────────────────────────────────────────
@@ -20,59 +22,70 @@
     const renderer =
         new TextKit.Environments.ANSITerminal.ANSITerminalStyleRenderer( )
 
-//
-// ─── CONSTANTS ──────────────────────────────────────────────────────────────────
-//
-
-    const HPadding = 6
-    const VPadding = 2
-    const WIDTH = process.stdout.columns - HPadding * 2
-    const HEIGHT = process.stdout.rows - 2 - VPadding * 2
 
 //
 // ─── GRAPH VIEW ─────────────────────────────────────────────────────────────────
 //
 
-    const graph = new TextKit.GraphView({
-        renderer,
-        width:      WIDTH,
-        height:     HEIGHT,
-        style:      { textColor: "cyan" },
-        scale:      0.01,
-        formula:    ( x, y, width ) => {
-            const a = ( x / width )
-            const s = Math.sin( a ) * 22
-            return y < ( Math.sin( s ) / 13 )
+    async function generateGraph ( iteration: number ) {
+        const HPadding = 6
+        const VPadding = 2
+        const WIDTH = process.stdout.columns - HPadding * 2
+        const HEIGHT = process.stdout.rows - VPadding * 2 - 1
+        const GRAPH_COLOR = "cyan"
+        const GUIDES_COLOR = "red"
+
+        const graph = new TextKit.GraphView({
+            renderer,
+            width:      WIDTH,
+            height:     HEIGHT,
+            style:      { textColor: GRAPH_COLOR },
+            zoom:       Math.PI * 3,
+            formula:    ( x, y, width ) => {
+                const thickness =
+                    0.12
+                const theta =
+                    Math.sin( ( x - 0.5 ) * iteration / width )
+                return ( y < theta + thickness ) && ( y > theta - thickness )
+            }
+        })
+
+
+        const hLine =
+            new TextKit.LineView( "─".repeat(WIDTH - 1) + "▶︎", renderer, { textColor: GUIDES_COLOR })
+        const vLineLines =
+            [ "▲" ]
+        for ( let row = 1; row < HEIGHT; row++ ) {
+            vLineLines.push( "│" )
         }
-    })
+        const vLine =
+            new TextKit.ShapeView( vLineLines, 0, renderer, { textColor: GUIDES_COLOR }, false )
 
+        const canvas =
+            new TextKit.CanvasView( WIDTH, HEIGHT, renderer )
+        const background =
+            new TextKit.CanvasView( WIDTH, HEIGHT, renderer )
 
-    const hLine =
-        new TextKit.LineView( "─".repeat(WIDTH - 1) + "▶︎", renderer, { textColor: "blue" })
-    const vLineLines =
-        [ "▲" ]
-    for ( let row = 1; row < HEIGHT; row++ ) {
-        vLineLines.push( "│" )
+        background.add( hLine, 0, Math.floor( HEIGHT / 2 ) + 1, 1 )
+        background.add( vLine, Math.floor( WIDTH / 2 ), 0, 2 )
+        background.fineTuneBoxIntersections( )
+
+        canvas.add( background, 0, 0, 1 )
+        canvas.add( graph, 0, 0, 2 )
+
+        const margined =
+            canvas.applyMargin( VPadding, HPadding, 0, HPadding )
+
+        console.clear( )
+        console.log( margined.styledForm )
+        Tools.setCursorToBottomRight( "TextKit GraphView Demo " )
+        await Tools.sleep( 100 )
     }
-    const vLine =
-        new TextKit.ShapeView( vLineLines, 0, renderer, { textColor: "blue" }, false)
 
-    const canvas =
-        new TextKit.CanvasView( WIDTH, HEIGHT, renderer )
-    const background =
-        new TextKit.CanvasView( WIDTH, HEIGHT, renderer )
+//
+// ─── MAIN ───────────────────────────────────────────────────────────────────────
+//
 
-    background.add( hLine, 0, Math.floor( HEIGHT / 2 ) + 1, 1 )
-    background.add( vLine, Math.floor( WIDTH / 2 ), 0, 2 )
-    background.fineTuneBoxIntersections( )
-
-    canvas.add( background, 0, 0, 1 )
-    canvas.add( graph, 0, 0, 2 )
-
-
-
-    const margined = canvas.applyMargin( VPadding, HPadding, VPadding, HPadding )
-
-    console.log( margined.styledForm )
+    Tools.runRenderLoop( 1, 20, generateGraph )
 
 // ────────────────────────────────────────────────────────────────────────────────
